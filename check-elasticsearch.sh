1aa1f6dea4a0c366567834f14cb6141e432ad9b8
b-long <b-long@users.noreply.github.com>
2015-10-02 11:16:46 -0400

    Initial commit of bash scripts to use AWS CloudWatch

---
+++ check-elasticsearch.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# More information is available : https://gist.github.com/b-long/632c310453b44fc8e785 
+
+# If you ever need to confirm that AWS is configured properly, invoke 'aws help' at the
+# command line.  This will also verify authentication. 
+
+# At installation time, make sure to set the initial alarm state:
+# aws cloudwatch set-alarm-state --alarm-name support_search-down --state-value OK --state-reason "Installing job"
+
+#set -x
+RETVAL=1
+
+echo "Checking if ElasticSearch is running" 
+
+if service elasticsearch status > /dev/null
+then
+    REASON="ElasticSearch is running"
+    echo "${REASON}"
+    RETVAL=0 # "OK"
+else
+    REASON="ElasticSearch is not running"
+    echo "${REASON}"
+    RETVAL=1 # "ALARM"
+fi
+
+/usr/local/bin/aws cloudwatch put-metric-data --metric-name ElasticSearchStatus --namespace "support.bioconductor.org" --value "${RETVAL}"
+
+echo "Status: ${RETVAL} sent to CloudWatch at $(date -u)."
+exit 0
+
